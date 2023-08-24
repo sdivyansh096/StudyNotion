2579b56862f9ce08251a6c6ab4307017f22ed010
Divyansh Sharma <138120857+sdivyansh096@users.noreply.github.com>
2023-08-24 09:17:26 +0530

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Dashboard = () => {
   return (
-    <div className='flex justify-center items-center text-white text-2xl h-full' >
+    <div className='flex justify-center items-center text-white text-2xl h-screen' >
       Welcome to StudyNotion
       </div>
   )
