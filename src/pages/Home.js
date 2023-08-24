9663c49b5fe50944cf715c41d743cb20d1c00689
Divyansh Sharma <138120857+sdivyansh096@users.noreply.github.com>
2023-08-24 09:18:18 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home({isloggedIn}) {
   return (
-    <div className='flex justify-center items-center text-white text-2xl h-full'>
+    <div className='flex justify-center items-center text-white text-2xl h-screen'>
     Home
     </div>
   )
